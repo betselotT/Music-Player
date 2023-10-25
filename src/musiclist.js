dd9f7203b44e03c9e41694a04e54515c6ac32d9d
betsiT <betselottesfa9@gmail.com>
2023-10-25 15:56:52 +0300

    first commit

---
+++ src/musiclist.js
@@ -0,0 +1,52 @@
+import {writable} from "svelte/store";
+
+export const musicList = writable([
+    {
+        image:"Dont_Stop.jpg",
+        audio:"Dont_Stop.mp3",
+        name:"Don't Stop",
+        artist:"5 Seconds of Summer"
+    },
+    {
+        image:"Easier.jpg",
+        audio:"Easier.mp3",
+        name:"Easier",
+        artist:"5 Seconds of Summer"
+    },
+    {
+        image:"Lie_To_Me.jpg",
+        audio:"Lie_To_Me.mp3",
+        name:"Lie To Me",
+        artist:"5 Seconds of Summer"
+    },
+    {
+        image:"Lonely_Heart.jpg",
+        audio:"Lonely_Heart.mp3",
+        name:"Lonely Heart",
+        artist:"5 Seconds of Summer"
+    },
+    {
+        image:"Lover_Of_Mine.jpg",
+        audio:"Lover_Of_Mine.mp3",
+        name:"Lover Of Mine",
+        artist:"5 Seconds of Summer"
+    },
+    {
+        image:"Teeth.jpg",
+        audio:"Teeth.mp3",
+        name:"Teeth",
+        artist:"5 Seconds of Summer"
+    },
+    {
+        image:"Valentine.jpg",
+        audio:"Valentine.mp3",
+        name:"Valentine",
+        artist:"5 Seconds of Summer"
+    },
+    {
+        image:"Wildflower.jpg",
+        audio:"Wildflower.mp3",
+        name:"Wildflower",
+        artist:"5 Seconds of Summer"
+    },
+]);
